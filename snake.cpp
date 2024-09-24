f699f15666381b8d7be0c8d44dad8230331f9ea3
Ishika Singh <144758046+ishikasingh21@users.noreply.github.com>
2024-09-24 21:49:31 +0530

    Update snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -8,28 +8,20 @@ const int height = 20;
 
 // Snake head coordinates of snake (x-axis, y-axis)
 int x, y;
-
 // Food coordinates
 int fruitCordX, fruitCordY;
-
 // variable to store the score of the player
 int playerScore;
-
 // Array to store the coordinates of snake tail (x-axis, y-axis)
 int snakeTailX[100], snakeTailY[100];
-
 // variable to store the length of the snake's tail
 int snakeTailLen;
-
 // for storing snake's moving snakesDirection
 enum snakesDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
-
 // snakesDirection variable
 snakesDirection sDir;
-
 // boolean variable for checking game is over or not
 bool isGameOver;
-
 // Variable to store the reason for losing
 string loseReason;
 
@@ -49,7 +41,6 @@ void GameInit()
 void GameRender(string playerName)
 {
     system("cls"); // Clear the console
-
     // Creating top walls with '-'
     for (int i = 0; i < width + 2; i++)
         cout << "-";
@@ -81,12 +72,62 @@ void GameRender(string playerName)
         }
         cout << endl;
     }
-
     // Creating bottom walls with '-'
     for (int i = 0; i < width + 2; i++)
         cout << "-";
     cout << endl;
-
     // Display player's score
     cout << playerName << "'s Score: " << playerScore << endl;
+}
+
+void UpdateGame()
+{
+    int prevX = snakeTailX[0];
+    int prevY = snakeTailY[0];
+    int prev2X, prev2Y;
+    snakeTailX[0] = x;
+    snakeTailY[0] = y;
+
+    for (int i = 1; i < snakeTailLen; i++) {
+        prev2X = snakeTailX[i];
+        prev2Y = snakeTailY[i];
+        snakeTailX[i] = prevX;
+        snakeTailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    switch (sDir) {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    }
+    // Checks for snake's collision with the wall (|)
+    if (x >= width || x < 0 || y >= height || y < 0) {
+        isGameOver = true;
+        loseReason = "You hit a wall!";
+    }
+    // Checks for collision with the tail (o)
+    for (int i = 0; i < snakeTailLen; i++) {
+        if (snakeTailX[i] == x && snakeTailY[i] == y) {
+            isGameOver = true;
+            loseReason = "You collided with your own tail!";
+        }
+    }
+    // Checks for snake's collision with the food (#)
+    if (x == fruitCordX && y == fruitCordY) {
+        playerScore += 1;
+        fruitCordX = rand() % width;
+        fruitCordY = rand() % height;
+        snakeTailLen++;
+    }
 }
