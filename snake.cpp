2d275891972ad1350d5650b7af36eff9a36c360d
Ishika Singh <144758046+ishikasingh21@users.noreply.github.com>
2024-09-24 21:48:17 +0530

    Update snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -45,3 +45,48 @@ void GameInit()
     playerScore = 0;
     snakeTailLen = 0;  // Initialize the snake's tail length
 }
+
+void GameRender(string playerName)
+{
+    system("cls"); // Clear the console
+
+    // Creating top walls with '-'
+    for (int i = 0; i < width + 2; i++)
+        cout << "-";
+    cout << endl;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j <= width; j++) {
+            // Creating side walls with '|'
+            if (j == 0 || j == width)
+                cout << "|";
+            // Creating snake's head with 'O'
+            if (i == y && j == x)
+                cout << "O";
+            // Creating the snake's food with '#'
+            else if (i == fruitCordY && j == fruitCordX)
+                cout << "#";
+            // Creating snake's tail with 'o'
+            else {
+                bool prTail = false;
+                for (int k = 0; k < snakeTailLen; k++) {
+                    if (snakeTailX[k] == j && snakeTailY[k] == i) {
+                        cout << "o";
+                        prTail = true;
+                    }
+                }
+                if (!prTail)
+                    cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    // Creating bottom walls with '-'
+    for (int i = 0; i < width + 2; i++)
+        cout << "-";
+    cout << endl;
+
+    // Display player's score
+    cout << playerName << "'s Score: " << playerScore << endl;
+}
