efe45296f713e224663239b84f3918809e10455b
Ishika Singh <144758046+ishikasingh21@users.noreply.github.com>
2024-09-24 21:46:14 +0530

    Update snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -5,3 +5,30 @@ using namespace std;
 
 const int width = 80;
 const int height = 20;
+
+// Snake head coordinates of snake (x-axis, y-axis)
+int x, y;
+
+// Food coordinates
+int fruitCordX, fruitCordY;
+
+// variable to store the score of the player
+int playerScore;
+
+// Array to store the coordinates of snake tail (x-axis, y-axis)
+int snakeTailX[100], snakeTailY[100];
+
+// variable to store the length of the snake's tail
+int snakeTailLen;
+
+// for storing snake's moving snakesDirection
+enum snakesDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+
+// snakesDirection variable
+snakesDirection sDir;
+
+// boolean variable for checking game is over or not
+bool isGameOver;
+
+// Variable to store the reason for losing
+string loseReason;
